ea11e69a83aa0a7621540190a8624adf56973f93
isadora <isadora.izlou@gmail.com>
2025-08-05 14:55:05 -0300

    criação do objeto...

--- cadastro-jpa/src/main/java/br/com/example/Main.java
+++ cadastro-jpa/src/main/java/br/com/example/Main.java
@@ -1,7 +1,19 @@
 package br.com.example;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Pessoa p1 = new Pessoa();
+        p1.setNome("isadora");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("meuPU");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(em);
     }
 }
