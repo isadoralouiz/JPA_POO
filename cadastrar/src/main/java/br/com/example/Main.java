df6c1a63c1eeb787ef9d5f13d372a2b207592f93
isadora <isadora.izlou@gmail.com>
2025-08-11 07:52:21 -0300

    UPDATE

---
+++ cadastrar/src/main/java/br/com/example/Main.java
@@ -0,0 +1,32 @@
+package br.com.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Ana");
+
+        // Cria o EntityManagerFactory com base na unidade de persistência 'meuPU'
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("meuPU");
+        EntityManager em = emf.createEntityManager();
+
+        // Inicia a transação
+        em.getTransaction().begin();
+
+        // Persiste o objeto no banco de dados
+        em.persist(p1);
+
+        // Finaliza a transação
+        em.getTransaction().commit();
+
+        // Fecha o EntityManager e o EntityManagerFactory
+        em.close();
+        emf.close();
+
+        System.out.println("Pessoa salva com sucesso!");
+    }
+}
